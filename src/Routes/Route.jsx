3ce8d14aec2f586eb210a958f2eaa07c9dccc8a4
sourish chatterjee <chatterjeesourishchatterjee@gmail.com>
2025-09-18 13:04:31 +0530

    test

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../pages/Login/Login";
 import Layout from "../../components/common/Layout";
-import Dashboard from "../../pages/Dashboard";
+import Dashboard from "../pages/Dashboard";
+import Graphview from "../pages/GraphView/Graphview";
 
 function AppRoutes() {
   return (
@@ -11,17 +12,29 @@ function AppRoutes() {
       <Routes>
         {/* Default route - redirect to dashboard */}
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        
+
         {/* Login route - standalone without layout */}
         <Route path="/login" element={<Login />} />
-        
+
         {/* Dashboard route with layout wrapper */}
-        <Route path="/dashboard" element={
-          <Layout>
-            <Dashboard />
-          </Layout>
-        } />
-        
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/graphview"
+          element={
+            <Layout>
+              <Graphview/>
+            </Layout>
+          }
+        />
+
         {/* Catch-all route for 404 */}
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
